ba25f60be0ffcfa018d9d81cfc59868b3c5146ea
joroonline <jo-ro-online@web.de>
2022-08-16 00:18:38 +0200

    Today

--- englisch.py
+++ englisch.py
@@ -1,6 +1,28 @@
-import sqlite3
+#import sqlite3
 
-connection = sqlite3.connect('Database_english_german')
-cursur = connection.cursor()
+def database():
+    #connection = sqlite3.connect('Database_english_german')
+    #cursur = connection.cursor()
+    #return cursur
+    pass
 
-cursur.execute()
+def new_vocabulary():
+    while True:
+        german = str(input("Please write a german word in this line: "))
+        english = str(input("Please write a english word in this line: "))
+        try:
+            return [german, english]
+        except ValueError:
+            print("Please write this words again!")
+
+def get_id():
+    pass
+
+#def new_voc_init(new_vocabulary):
+#    for x, y in enumerate(new_vocabulary):
+#        print(f"{x+1}. {y}")
+
+if __name__ == "__main__":
+    print(x for x, y in enumerate(new_vocabulary()))
+
+#cursur.execute()
--- test.py
+++ test.py
@@ -1,23 +1,3 @@
-def get_input():
-    _get = int(input())
-    try:
-        return _get
-    except ValueError:
-        print("Please try again!")
+import random
 
-def square_root(_input):
-    count = 1
-    for i in range(_input):
-        count += 2
-        if count >= _input:
-            if count > _input:
-                i -= 1
-                print(i)
-                break
-            if count == _input:
-                print(i)
-                break
-
-if __name__ == "__main__":
-    _input = get_input()
-    square_root(_input)
+print([f'{x+1}. {y}' for x, y in enumerate([random.randint(1,9) for i in range(10)])])
