f2200c655a4bca5f8ab000811848fdd02910b19a
joroonline <jo-ro-online@web.de>
2022-08-17 23:16:10 +0200

    17.08.2022
    new class

--- englisch.py
+++ englisch.py
@@ -1,4 +1,4 @@
-#import sqlite3
+import sqlite3
 
 def get_input():
     english = str(input("Please write a english word in this line: "))
@@ -11,12 +11,22 @@ def get_input():
 class Vocabulary():
 
     def __init__(self, vocabulary=None):
+        self.connect = sqlite3.connect('Database_englisch_german')
+        self.cusor = self.connect.cursor()
+        self.combo = 0
+        self.id  = 0
         if vocabulary is None:
             vocabulary = get_input()
         for s in range(len(vocabulary)):
             self.english = vocabulary[0]
             self.german = vocabulary[1]
 
+    def init_vocabulary(self):
+        self.cusor.execute('INSERT INTO zeitstempe (zeit) VALUES (?)', [self.english, self.german, self.combo])
+
+    def get_vocabulary(self):
+        pass
+
 get = Vocabulary()
 
-print(get.german)
+print(get.init_vocabulary())
